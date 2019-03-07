7b18019696b55be8d858e78e818f1e63ada80f7b
Trent Bitterman <trentbitterman@comcast.net>
2019-03-06 19:19:17 -0700

    Split main method into separate functions

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,31 @@
 extern crate clap;
 extern crate rand;
+
 use clap::{App, Arg};
 use rand::distributions::Uniform;
 use rand::Rng;
 
+struct Parameters {
+    number_of_dice: u32,
+    number_of_sides: u32,
+}
+
 fn main() {
-    let matches = make_args();
+    let matches = generate_argument_matcher();
 
-    let number_of_dice = matches.value_of("number").unwrap_or("1");
-    let number_of_sides = matches.value_of("sides").unwrap_or("6");
-
-    let number_of_dice: u32 = number_of_dice
-        .parse()
-        .expect("The number of dice must be a positive, non-zero number");
-
-    let number_of_sides: u32 = number_of_sides
-        .parse()
-        .expect("The number of sides must be a positive, non-zero number");
+    let params = get_cl_parameters(&matches);
 
     println!(
         "The number of dice is: {} and the number of sides is: {}",
-        number_of_dice, number_of_sides
+        params.number_of_dice, params.number_of_sides
     );
 
-    let mut rng = rand::thread_rng();
+    let rolls = roll_dice(params.number_of_dice, params.number_of_sides);
 
-    let range_of_dice = Uniform::new_inclusive(1, number_of_sides);
-
-    let mut die_roll = rng.sample_iter(&range_of_dice);
-
-    let mut rolls: Vec<u32> = Vec::new();
-
-    for _ in 0..number_of_dice {
-        rolls.push(die_roll.next().unwrap());
-    }
-
-    for roll in rolls.iter() {
-        print!("{} ", roll);
-    }
-    println!("");
+    output_rolls(&rolls);
 }
 
-fn make_args() -> clap::ArgMatches<'static> {
+fn generate_argument_matcher() -> clap::ArgMatches<'static> {
     App::new("dice")
         .version("0.1")
         .author("Trent B. <trentbitterman@comcast.net>")
@@ -63,4 +47,45 @@ fn make_args() -> clap::ArgMatches<'static> {
                 .takes_value(true),
         )
         .get_matches()
+}
+
+fn get_cl_parameters(matches: &clap::ArgMatches) -> Parameters {
+    let number_of_dice = matches.value_of("number").unwrap_or("1");
+    let number_of_sides = matches.value_of("sides").unwrap_or("6");
+
+    let number_of_dice: u32 = number_of_dice
+        .parse()
+        .expect("The number of dice must be a positive, non-zero number");
+
+    let number_of_sides: u32 = number_of_sides
+        .parse()
+        .expect("The number of sides must be a positive, non-zero number");
+
+    Parameters {
+        number_of_dice,
+        number_of_sides,
+    }
+}
+
+fn roll_dice(number_of_dice: u32, number_of_sides: u32) -> Vec<u32> {
+    let mut rng = rand::thread_rng();
+
+    let range_of_dice = Uniform::new_inclusive(1, number_of_sides);
+
+    let mut die_roll = rng.sample_iter(&range_of_dice);
+
+    let mut rolls: Vec<u32> = Vec::new();
+
+    for _ in 0..number_of_dice {
+        rolls.push(die_roll.next().unwrap());
+    }
+
+    rolls
+}
+
+fn output_rolls(rolls: &Vec<u32>) {
+    for roll in rolls.iter() {
+        print!("{} ", roll);
+    }
+    println!("");
 }
