3f1f22ae8998e272548d269a402ff3792803851e
Trent Bitterman <trentbitterman@comcast.net>
2019-03-11 17:37:27 -0600

    Added input validation for number and sides

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,15 @@ fn generate_argument_matcher() -> clap::ArgMatches<'static> {
                 .long("number")
                 .value_name("N")
                 .help("The number of dice to roll.")
-                .takes_value(true),
+                .takes_value(true)
+                .validator(|v| {
+                    if v.parse::<u32>().is_ok() {
+                        return Ok(());
+                    }
+                    Err(String::from(
+                        "The value should be an integer greater than or equal to 0.",
+                    ))
+                }),
         )
         .arg(
             Arg::with_name("sides")
@@ -44,7 +52,15 @@ fn generate_argument_matcher() -> clap::ArgMatches<'static> {
                 .long("sides")
                 .value_name("N")
                 .help("How many sides the dice or die should have.")
-                .takes_value(true),
+                .takes_value(true)
+                .validator(|v| {
+                    if v.parse::<u32>().is_ok() && v.parse::<u32>().unwrap() > 0 {
+                        return Ok(());
+                    }
+                    Err(String::from(
+                        "The value should be an integer greater than 0.",
+                    ))
+                }),
         )
         .arg(
             Arg::with_name("glyphs").short("g").long("glyphs").help(
@@ -61,13 +77,8 @@ fn get_cl_parameters() -> Parameters {
     let number_of_sides = matches.value_of("sides").unwrap_or("6");
     let glyphs = matches.is_present("glyphs");
 
-    let number_of_dice: u32 = number_of_dice
-        .parse()
-        .expect("The number of dice must be a positive, non-zero number");
-
-    let number_of_sides: u32 = number_of_sides
-        .parse()
-        .expect("The number of sides must be a positive, non-zero number");
+    let number_of_dice: u32 = number_of_dice.parse().unwrap();
+    let number_of_sides: u32 = number_of_sides.parse().unwrap();
 
     Parameters {
         number_of_dice,
